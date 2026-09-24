a0e0ec0e7baffee9476289781cd23b156e6c7fe0
designbyalyc <aliciachowxw@gmail.com>
2026-09-24 03:34:53 -0700

    Update website

--- script.js
+++ script.js
@@ -5,6 +5,28 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
+
+    /* ================= HERO IMAGE SLIDER ================= */
+
+const heroSlides = document.querySelectorAll(".hero-slide");
+
+if (heroSlides.length > 1) {
+
+    let currentSlide = 0;
+
+    setInterval(() => {
+
+        heroSlides[currentSlide].classList.remove("active");
+
+        currentSlide =
+            (currentSlide + 1) % heroSlides.length;
+
+        heroSlides[currentSlide].classList.add("active");
+
+    }, 5000);
+
+}
+
     /* ================= HEADER ================= */
 
     const header = document.getElementById("siteHeader");
